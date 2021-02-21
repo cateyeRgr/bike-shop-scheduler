285961e4100121957c329f17c346b3e106d425da
cateyeRgr <74964267+cateyeRgr@users.noreply.github.com>
2021-02-21 01:57:33 +0100

    Delete, da in eine Datei zusammengelegt s. Tabellen anlegen

--- documentation/SQLQuerySOTProjektCustomer.sql
+++
@@ -1,9 +0,0 @@
-use Terminerstellung
-go
-
-Insert into Customer(LastName, FirstName, Postleitzahl, Ort, Straﬂe, Hausnummer)
-Values ('Doe', 'John', 66600, 'Nowhere', 'Elmstreet','12a'),
-	   ('Smith', 'Jane', 00001, 'Castle Rock', 'Main Street','11111'),
-	   ('Picard', 'Jean-Luc', 70190, 'La Barre', 'Grande Rue','3'),
-       ('Scully', 'Dana', 20906, 'Bethesda', '3170 West 53 Rd','35'); 
-
